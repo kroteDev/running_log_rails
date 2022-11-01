390e750b93c02c9e4ed892b96ac4bfa890a06af7
bolliveira <bruno.oliveira@fiocruz.br>
2022-11-01 13:15:42 -0300

    Ajustes no formulário de atividades e login

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,8 @@
 class Activity < ApplicationRecord
   belongs_to :user
   validates_presence_of :activity_type, :distance
+
+  def self.activity_type_options
+    {"walk" => 0, "Running" => 1, "Jogging" => 2, "Marathon" => 3, "Public Run" => 4}
+  end
 end
